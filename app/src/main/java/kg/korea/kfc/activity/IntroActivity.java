d76b1a07669699fc0a3694eac6efd5d237b95776
freesoul1983@gmail.ocm <water830>
2017-03-28 21:08:40 +0900

    TODO 리스트 추가

--- app/src/main/java/kg/korea/kfc/activity/IntroActivity.java
+++ app/src/main/java/kg/korea/kfc/activity/IntroActivity.java
@@ -4,7 +4,6 @@ package kg.korea.kfc.activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import kg.korea.kfc.R;
 import kg.korea.kfc.databinding.ActivityIntroBinding;
@@ -19,6 +18,10 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // TODO 글라이드 라이브러리 추가
+        // TODO 레트로핏2 추가
+        // 네이버, 페이스북 간편 로그인 추가
 
         ActivityIntroBinding activityIntroBinding = DataBindingUtil.setContentView(this, R.layout.activity_intro);
 
